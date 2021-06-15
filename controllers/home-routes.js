fbf15f4c2475f706b04517012a8b684cb249117b
ciyer87 <chitra.iyer00@gmail.com>
2021-06-15 16:59:57 -0700

    added seeds

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -3,16 +3,36 @@ const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
 
 router.get('/', (req, res) => {
-    res.render('homepage', {
-      id: 1,
-      post_text: 'lorem ipsum',
-      title: 'Handlebars Docs',
-      created_at: new Date(),
-      comments: [{}, {}],
-      user: {
-        username: 'test_user'
-      }
-    });
+    Post.findAll({
+      attributes: [
+        'id',
+        'post_text',
+        'title',
+        'created_at', 
+      ],
+      include: [
+        {
+          model: Comment,
+          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+          include: {
+            model: User,
+            attributes: ['username']
+          }
+        },
+        {
+          model: User,
+          attributes: ['username']
+        }
+      ]
+    })
+      .then(dbPostData => {
+        // pass a single post object into the homepage template
+        res.render('homepage', dbPostData[0].get({ plain: true }));
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
   });
 
 module.exports = router;
---
+++ seeds/index.js
@@ -0,0 +1,26 @@
+const seedUsers = require('./user');
+const seedPosts = require('./post');
+const seedComments = require('./comment');
+//const seedVotes = require('./vote-seeds');
+
+const sequelize = require('../config/connection');
+
+const seedAll = async () => {
+  await sequelize.sync({ force: true });
+  console.log('--------------');
+  await seedUsers();
+  console.log('--------------');
+
+  await seedPosts();
+  console.log('--------------');
+
+  await seedComments();
+  console.log('--------------');
+
+  await seedVotes();
+  console.log('--------------');
+
+  process.exit(0);
+};
+
+seedAll();
