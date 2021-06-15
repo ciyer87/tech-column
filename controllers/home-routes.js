2ade812addd67266a5c9740fb4f4f0fdd40b7d94
ciyer87 <chitra.iyer00@gmail.com>
2021-06-15 14:15:26 -0700

    added handlebars initial files

---
+++ controllers/home-routes.js
@@ -0,0 +1,18 @@
+const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Post, User, Comment } = require('../models');
+
+router.get('/', (req, res) => {
+    res.render('homepage', {
+      id: 1,
+      post_text: 'lorem ipsum',
+      title: 'Handlebars Docs',
+      created_at: new Date(),
+      comments: [{}, {}],
+      user: {
+        username: 'test_user'
+      }
+    });
+  });
+
+module.exports = router;
