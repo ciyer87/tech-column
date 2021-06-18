17717555e1ce35413a2c059a9cd92dfd97bbb829
ciyer87 <chitra.iyer00@gmail.com>
2021-06-17 20:22:56 -0700

    added conditions to render login /logout link

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -28,7 +28,7 @@ router.get('/', (req, res) => {
       .then(dbPostData => {
         // pass a single post object into the homepage template
         const posts = dbPostData.map(post => post.get({ plain: true }));
-        res.render('homepage', { posts });
+        res.render('homepage', { posts, loggedIn: req.session.loggedIn });
       })
       .catch(err => {
         console.log(err);
@@ -87,7 +87,7 @@ router.get('/', (req, res) => {
         const post = dbPostData.get({ plain: true });
   
         // pass data to template
-        res.render('single-post', { post });
+        res.render('single-post', { post, loggedIn: req.session.loggedIn });
       })
       .catch(err => {
         console.log(err);
