96867697e1a12a5bc7c52b515962fa2ddacfe26b
ciyer87 <chitra.iyer00@gmail.com>
2021-06-17 21:18:12 -0700

    added partials and helper files

--- server.js
+++ server.js
@@ -5,13 +5,14 @@ const sequelize = require('./config/connection');
 const exphbs = require('express-handlebars');
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const helpers = require('./utils/helpers');
 
 
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ helpers });
 const sess = {
     secret: process.env.DB_SECRET,
     cookie: {},
