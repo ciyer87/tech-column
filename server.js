d18fccae39c9b2e430ad6d00525433cff97bea38
ciyer87 <chitra.iyer00@gmail.com>
2021-06-19 13:00:57 -0700

    updated session secret

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const PORT = process.env.PORT || 3001;
 
 const hbs = exphbs.create({ helpers });
 const sess = {
-    secret: process.env.DB_SECRET,
+    secret: process.env.SECRET_SESSION,
     cookie: {
         expires: 30*60*1000
     },
